1524e5e20f8f0bedd9b6efa9068da6bf48857c50
Fabian Canas <fcanas@gmail.com>
2016-08-27 20:40:48 -0400

    Enable creation of relieve URLs by extracting common prefix
    Relative URLs have supported making a resource URL relative to a
    directory. But it was not possible to support a resource URL relative to
    another resource URL. This includes support for ../ changes in relative
    paths.

--- Sources/URL.swift
+++ Sources/URL.swift
@@ -14,15 +14,50 @@ extension URL {
      * Called on an absolute URL, returns a new relative URL based on the parameter.
      * for example:
      * (http://example.com/a/test).relativeURL(http://example.com/a/) -> test
+     *
+     * [RFC 1808](https://tools.ietf.org/html/rfc1808) is relevant, but not strictly followed.
      */
     func relativeURL(baseURL: URL) -> URL {
         let baseString = baseURL.absoluteString
         let selfString = absoluteString
+        
         if selfString.hasPrefix(baseString) {
             var relativeString = selfString
             relativeString.removeSubrange(baseString.fullRange)
             return URL(string: relativeString, relativeTo: baseURL) ?? self
+        } else {
+            
+            let commonPrefix = selfString.commonPrefix(with: baseString)
+            
+            // By checking the scheme, user, host, and port are equal, we avoid
+            // the problem of creating relative URLs where they're not 
+            // appropriate, and that further checking is effectively on the path
+            guard baseURL.scheme == self.scheme && baseURL.user == self.user && baseURL.host == self.host && baseURL.port == self.port else {
+                return self
+            }
+            
+            // TODO : how to handle query and fragments?
+            
+            let strippedBaseString = baseString.replacingCharacters(in: commonPrefix.fullRange, with: "")
+            let strippedSelfString = selfString.replacingCharacters(in: commonPrefix.fullRange, with: "")
+            
+            let numberOfDirectorySlashes = strippedBaseString.characters.reduce(0, { (count :Int, character) -> Int in
+                if character == "/" {
+                    return count + 1
+                }
+                return count
+            })
+            
+            var prefix = ""
+            for _ in 0..<numberOfDirectorySlashes {
+                prefix += "../"
+            }
+            
+            if let result = URL(string: prefix + strippedSelfString, relativeTo: baseURL) {
+                return result
+            }
         }
+        
         return self
     }
     
--- Tests/HLSCoreTests/SerializationTests.swift
+++ Tests/HLSCoreTests/SerializationTests.swift
@@ -16,16 +16,16 @@ class SerializationTests: XCTestCase {
         let urlString = "http://example.com/test/playlist.m3u8"
         
         let s1URL = URL(string: "http://example.com/test/s1.ts")!
-        let s2URL = URL(string: "http://example.com/test/s2.ts")!
-        let s3URL = URL(string: "http://example.com/test/s3.ts")!
-        let s4URL = URL(string: "http://example.com/test/s4.ts")!
+        let s2URL = URL(string: "s2.ts")!
+        let s3URL = URL(string: "http://example.com/s3.ts")!
+        let s4URL = URL(string: "http://example.com/test/alt/s4.ts")!
         
         let segments = [MediaSegment(uri: s1URL, duration: 3.1),
                         MediaSegment(uri: s2URL, duration: 3.0),
                         MediaSegment(uri: s3URL, duration: 3.2),
                         MediaSegment(uri: s4URL, duration: 2.9)]
         
-        let playlist :MediaPlaylist = MediaPlaylist(type: .VOD, version: 3, uri: URL(string: urlString)!, targetDuration: 12, closed: true, start: nil, segments: segments)
+        let playlist :MediaPlaylist = MediaPlaylist(type: .VOD, version: 3, uri: URL(string: urlString)!, targetDuration: 3, closed: true, start: nil, segments: segments)
         
         let stringResource = MediaPlaylistSerlializer().serialize(playlist)
         
@@ -33,7 +33,7 @@ class SerializationTests: XCTestCase {
         
         let expectedPlaylist =
             "#EXTM3U" + "\n" +
-                "#EXT-TARGET-DURATION:12" + "\n" +
+                "#EXT-TARGET-DURATION:3" + "\n" +
                 "#EXT-X-VERSION:3" + "\n" +
                 "#EXT-X-PLAYLIST-TYPE:VOD" + "\n" +
                 "#EXTINF:3.1" + "\n" +
@@ -41,9 +41,9 @@ class SerializationTests: XCTestCase {
                 "#EXTINF:3.0" + "\n" +
                 "s2.ts" + "\n" +
                 "#EXTINF:3.2" + "\n" +
-                "s3.ts" + "\n" +
+                "../s3.ts" + "\n" +
                 "#EXTINF:2.9" + "\n" +
-                "s4.ts" + "\n"
+                "alt/s4.ts" + "\n"
         
         AssertMatchMultilineString(stringResource.value!, expectedPlaylist, separator: "\n")
         
